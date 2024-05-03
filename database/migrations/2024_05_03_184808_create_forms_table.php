2f4db62b628cb7421be4224e5c14b4ff14ac157c
ThyagoMartins1 <oficialthyago2@gmail.com>
2024-05-03 15:56:11 -0300

    #

---
+++ database/migrations/2024_05_03_184808_create_forms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('email');
+            $table->text('description');
+            $table->text('hobbies');
+            $table->text('experience');
+            $table->text('impacto_social');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forms');
+    }
+};
